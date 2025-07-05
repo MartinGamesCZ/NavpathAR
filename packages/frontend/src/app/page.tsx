54fa08809e23833a9883d466a778f45ca0610069
Martin Petr <martinpetrnp@gmail.com>
2025-07-05 13:43:24 +0200

    Add box fixed to the ground

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,11 +1,33 @@
 "use client";
 
 import { Canvas, useFrame } from "@react-three/fiber";
-import { XR, createXRStore } from "@react-three/xr";
-import { useRef, useState } from "react";
+import { XR, createXRStore, useXR } from "@react-three/xr";
+import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 
 const store = createXRStore();
+
+function GroundPosition() {
+  const { origin } = useXR();
+  const groundPosition = useRef<THREE.Vector3 | null>(null);
+
+  useEffect(() => {
+    if (origin && groundPosition.current) {
+      groundPosition.current = origin.position;
+    }
+  }, [origin]);
+
+  return (
+    <group>
+      {groundPosition.current && (
+        <mesh position={groundPosition.current} scale={[0.1, 0.1, 0.1]}>
+          <boxGeometry />
+          <meshStandardMaterial color="red" />
+        </mesh>
+      )}
+    </group>
+  );
+}
 
 function Box() {
   const ref = useRef<THREE.Mesh>(null);
@@ -39,6 +61,7 @@ export default function App() {
           <ambientLight intensity={0.8} />
           <directionalLight intensity={5} position={[100, 60, 100]} />
           <Box />
+          <GroundPosition />
         </XR>
       </Canvas>
     </>
